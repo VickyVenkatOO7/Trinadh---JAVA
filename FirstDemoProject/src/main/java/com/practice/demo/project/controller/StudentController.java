afee6e159e3d05de2941fc035f448bb62d4bedc6
Trinadh Sai Rapeti <116419508+VickyVenkatOO7@users.noreply.github.com>
2024-01-22 09:23:46 +0530

    Add files via upload
    Assistance of Postman Related Annotations

--- FirstDemoProject/src/main/java/com/practice/demo/project/controller/StudentController.java
+++ FirstDemoProject/src/main/java/com/practice/demo/project/controller/StudentController.java
@@ -1,36 +1,45 @@
 package com.practice.demo.project.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.practice.demo.project.dao.*;
 import com.practice.demo.project.model.Students;
 
-@Controller
+@RestController
 public class StudentController {
 	
 	@Autowired
 	StudentsRepo repo;
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		return "Home.jsp";
 	}
 	
 	
-	@RequestMapping("/addStudents")
+	@GetMapping("/addStudents")
 	public String addStudents(Students students) {
 		repo.save(students);
 		return "Home.jsp";
 	}
 	
+	@PostMapping("/Students")
+	public Students addStudent(Students students) {
+		repo.save(students);
+		return students;
+	}
 	
-	@RequestMapping("/fetchStudentsById")
+	@GetMapping("/fetchStudentsById")
 	public ModelAndView fetchDetailsById(@RequestParam int stdId) {
 		ModelAndView mv = new ModelAndView("Details.jsp");
 		Optional<Students> students = repo.findById(stdId);
@@ -45,11 +54,24 @@ public class StudentController {
 	}
 	
 	
-	@RequestMapping("/fetchAllStudents")
+	@GetMapping("/fetchAllStudents")
 	public ModelAndView fetchAllDetails() {
 		ModelAndView mv = new ModelAndView("AllDetails.jsp");
 		Students students =(Students) repo.findAll();	
 		mv.addObject("students", students);
 		return mv;
+	}
+	
+	
+	
+	@GetMapping("/Students")
+	public List<Students> fetchStudents() {
+		return repo.findAll();
+	}
+	
+	
+	@GetMapping("/Student/{stdId}")
+	public Optional<Students> fetchStudent(@PathVariable("stdId") int stdId) {
+		return repo.findById(stdId);
 	}
 }
